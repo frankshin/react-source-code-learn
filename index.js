b5ec3c7e05872bf7b4914cdbfabedb5618533149
dengweixu <1103579820@qq.com>
2021-01-28 16:52:09 +0800

    add some console log

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ export default class IndexPage extends Component {
 
   render(){
     const { value } = this.state
-    // return <div>点击我{value}</div>
-    return <div onClick={() => { this.setStateValue() }}>点击我{value}</div>
+    return <div>点击我{value}</div>
+    // return <div onClick={() => { this.setStateValue() }}>点击我{value}</div>
   }
 
   componentDidMount(){
-  //   const _this = this
-  //   const parentDom = ReactDOM.findDOMNode(this)
-  //   parentDom.addEventListener('click', function(){
-  //     _this.setState({
-  //       value: 2
-  //     })
-  //     console.log('value', _this.state.value) // ？
-  //   }, false)
+    const _this = this
+    const parentDom = ReactDOM.findDOMNode(this)
+    parentDom.addEventListener('click', function(){
+      _this.setState({
+        value: 2
+      })
+      console.log('value', _this.state.value) // ？
+    }, false)
   }
 }
 
