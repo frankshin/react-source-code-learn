2dec96580cfda4b75c9c7dcd55e040ebc4277ac8
dengweixu <1103579820@qq.com>
2021-01-28 15:47:51 +0800

    add some console log

--- index.js
+++ index.js
@@ -9,32 +9,29 @@ export default class IndexPage extends Component {
     }
   }
 
-  testStateValue (){
+  setStateValue(){
     this.setState({
       value: 2
     })
-    console.log('value', this.state.value)
+    console.log('setStateValue value', this.state.value) // ？
   }
 
   render(){
     const { value } = this.state
-    return <div onClick={() => { this.testStateValue() }}>点击我{value}</div>
     // return <div>点击我{value}</div>
+    return <div onClick={() => { this.setStateValue() }}>点击我{value}</div>
   }
 
   componentDidMount(){
-    const _this = this
-    const parentDom = ReactDOM.findDOMNode(this)
-    // const dom = document.querySelector('div')
-    parentDom.addEventListener('click', function(){
-      // 此处的setstate赋值后value的变化就是同步的！！！！！！！！why？？？？？
-      _this.setState({
-        value: 2
-      })
-      console.log('value', _this.state.value)
-    }, false)
+  //   const _this = this
+  //   const parentDom = ReactDOM.findDOMNode(this)
+  //   parentDom.addEventListener('click', function(){
+  //     _this.setState({
+  //       value: 2
+  //     })
+  //     console.log('value', _this.state.value) // ？
+  //   }, false)
   }
-
 }
 
 ReactDOM.render(
