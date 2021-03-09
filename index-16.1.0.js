f222d521d339279f26b55cdadb3414a89246d56b
dengweixu <1103579820@qq.com>
2021-03-09 23:15:03 +0800

    add learn

--- index-16.1.0.js
+++ index-16.1.0.js
@@ -1,33 +1,61 @@
+// // setstate执行流测试
+// import React, {Component} from 'react';
+// import ReactDOM from 'react-dom'
+
+// class Main extends Component {
+//   constructor(){
+//     super()
+//     this.state={
+//       time: 1
+//     }
+//   }
+//   func1(){
+//     // this.setState({
+//     //   time: 2
+//     // })
+//     this.setState((state) => {
+//       return {
+//         time: state.time + 1
+//       }
+//     })
+//     console.log('time：', this.state.time)
+//   }
+//   handleClick() {
+//     // transMyaction.perform(this.func1, this)
+//     this.func1()
+//   }
+//   render() {
+//     const { time } = this.state
+//     return (
+//       <div>
+//         <button onClick={() => this.handleClick()}>say Hello{time}</button>      
+//       </div>
+//     );
+//   }
+// }
+// ReactDOM.render(
+//   <Main />,
+//   document.getElementById('root')
+// );
+
+
+
+// 父组件渲染 子组件渲染测试
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
+import One from './one'
 
 class Main extends Component {
-  constructor(){
+  constructor(props){
     super()
-    this.state={
-      time: 1
-    }
-  }
-  func1(){
-    // this.setState({
-    //   time: 2
-    // })
-    this.setState((state) => {
-      return {
-        time: state.time + 1
-      }
-    })
-    console.log('time：', this.state.time)
-  }
-  handleClick() {
-    // transMyaction.perform(this.func1, this)
-    this.func1()
+    this.state = {}
   }
   render() {
     const { time } = this.state
     return (
       <div>
-        <button onClick={() => this.handleClick()}>say Hello{time}</button>      
+        <button onClick={() => this.setState({ time: 2 })}>say Hello{time}</button>  
+        <One />    
       </div>
     );
   }
---
+++ one.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class One extends Component {
+  
+  render() {
+    console.log(11111111)
+    return <div>123456</div>
+  }
+}
+
+export default One
+
+
+
